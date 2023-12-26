2fa48c77ff87b9d09b36f23a29f7c7ea0febeb40
erasebegin <chris.james.haupt@gmail.com>
2023-12-26 22:29:16 +0000

    add contact page contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   buildOptions: {
-    site: 'http://localhost:4321',
+    site: 'http://localhost:4321'
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
