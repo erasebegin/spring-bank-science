5accc74ccea89c05177cb08c9d850d6be69aa501
erasebegin <chris.james.haupt@gmail.com>
2023-12-27 14:06:18 +0000

    attempt to get contact form working

--- src/components/ContactButtons.tsx
+++ src/components/ContactButtons.tsx
@@ -9,7 +9,16 @@ const ContactButtons = ({ phoneNumber }: { phoneNumber: string }) => {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    setOpenModal("sent");
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => setOpenModal("sent"))
+      .catch((error) => alert(error));
   };
 
   function getModalContents(current: string) {
